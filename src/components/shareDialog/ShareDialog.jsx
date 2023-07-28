ef602fd1a746923f9a80236ba6469f2f824f744c
Alexander Kostrikin <aleksandrkostrikin086@gmail.com>
2023-07-28 17:39:45 +0300

    Fix bug

--- src/components/shareDialog/ShareDialog.jsx
+++ src/components/shareDialog/ShareDialog.jsx
@@ -12,7 +12,7 @@ export default function ShareDialog(props) {
         <div className="share-wrapper">
             <div className="share">
                 <div className="links">
-                    <a href={require("../../imgs/" + props.image)} download={fileName}>Сохранить фото<img src={linkImg} alt="Ссылка" /></a>
+                    <a href={require("../../imgs/farm/" + props.image)} download={fileName}>Сохранить фото<img src={linkImg} alt="Ссылка" /></a>
                     <a href="#">Поделиться<img src={linkImg} alt="Ссылка" /></a>
                 </div>
                 <button className="button" onClick={props.buttonClickHandler}>Далее</button>
